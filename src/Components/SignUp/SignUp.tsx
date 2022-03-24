bde404e9e68af22b3a5a5acdb47e906efa61e1aa
sapirhirsch <hirsch.sapir@gmail.com>
2022-03-24 15:00:02 +0200

    add form

--- src/Components/SignUp/SignUp.tsx
+++ src/Components/SignUp/SignUp.tsx
@@ -1,13 +1,46 @@
+import { Grid, TextField } from '@material-ui/core';
 import { Title } from '@material-ui/icons';
 import React, { useState } from 'react';
 import { Wrapper } from './SignUp.styles';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import FormControl from '@material-ui/core/FormControl';
+import FormLabel from '@material-ui/core/FormLabel';
+import RadioGroup from '@material-ui/core/RadioGroup';
+import Radio from '@material-ui/core/Radio';
+import Select from '@material-ui/core/Select';
+import MenuItem from '@material-ui/core/MenuItem';
+import Slider from '@material-ui/core/Slider';
+import Button from '@material-ui/core/Button';
+import { SignUpFields } from '../../Types/signUp.types';
 
-const SignUp = () => {
+const SignUpForm = () => {
+  const [formValues, setFormValues] = useState<SignUpFields | {}>({});
+
+  const handleInputChange = (e: any) => {
+    const { name, value } = e.target;
+    setFormValues({
+      ...formValues,
+      [name]: value,
+    });
+  };
+  const handleComplexInputChange = (name: string) => (e: any, value: any) => {
+    setFormValues({
+      ...formValues,
+      [name]: value,
+    });
+  };
+  const handleSubmit = (event: any) => {
+    event.preventDefault();
+    console.log(formValues);
+  };
   return (
-    <Wrapper>
-      <h1>hello</h1>
-    </Wrapper>
+    <form onSubmit={handleSubmit}>
+      <Grid container alignItems='center' justify='center' direction='column'>
+        <Button variant='contained' color='primary' type='submit'>
+          Submit
+        </Button>
+      </Grid>
+    </form>
   );
 };
-
-export default SignUp;
+export default SignUpForm;
