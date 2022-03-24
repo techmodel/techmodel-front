6224472590830acc2eae04b6255d45a660812587
Nimrod Galor <galor_nimrod@hotmail.com>
2022-03-24 17:22:30 +0200

    done 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,70 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Offer from './components/Offer';
 import OfferList from './components/OfferList';
+import {config} from './config';
+import {PublicClientApplication} from '@azure/msal-browser';
+import { createLogicalNot } from 'typescript';
+import { LocalDiningOutlined } from '@material-ui/icons';
+import { render } from '@testing-library/react';
+import { Button } from '@material-ui/core';
+/* eslint-disable */
 
-const App = () => {
-  return <OfferList />;
+//const App = () => {
+class App extends Component{
+  publicClientApplication: PublicClientApplication;
+
+  constructor(props: any){
+    super(props);
+    this.state = {
+      error:"",
+      isAuthenticated:null,
+      user:{}
+    };
+  
+
+    this.login = (this as any).login.bind(this)
+
+    this.publicClientApplication = new PublicClientApplication({
+      auth:{
+        clientId:config.appId,
+        redirectUri:config.redirectUrl,
+        authority:config.authority
+      },
+      cache:{
+        cacheLocation:"sessionStorage",
+        storeAuthStateInCookie: true
+      }
+    });
+}
+  
+
+  async login(){
+    try{
+      await this.publicClientApplication.loginPopup({
+        scopes: config.scopes,
+        prompt: "select_account"
+      });
+      this.setState({isAuthenticated:true})
+    }
+    catch(err){
+      this.setState({
+        isAuthenticated:false,
+        user:{},
+        error:err
+      });
+    }
+  }
+
+  logout(){
+    this.publicClientApplication.logout();
+  }
+
+  render(){
+    return (this.state as any).isAuthenticated ?
+        <OfferList /> :
+        <button onClick={() => this.login()}>Log in</button>;
+  }
+
 };
 
 export default App;
---
+++ src/config.js
@@ -0,0 +1,8 @@
+export const config = {
+    appId:'71e01e95-aa1d-4f00-9e02-a4c3c47f0e7d',
+    redirectUrl:'http://localhost:3000',
+    scopes: [
+        'user.read',
+    ],
+    authority:'https://login.microsoftonline.com/greendreamteam.onmicrosoft.com',
+}
