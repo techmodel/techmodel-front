407fedcfa9510491bd8a29bf46836c7104b4b4ab
Gabriel L <gabriel@delitefully.com>
2022-04-26 17:16:11 +0300

    lint: pass build

--- src/components/FilterBar/FilterBar.tsx
+++ src/components/FilterBar/FilterBar.tsx
@@ -3,41 +3,34 @@ import FilterIcon from '@mui/icons-material/FilterList';
 
 import * as s from './FilterBar.styled';
 import FilterRow from './FilterRow';
-import StringFilter from './FilterRow/filterInputs/StringFilter/StringFilter';
+import StringFilter from './FilterRow/filterInputs/stringFilter/StringFilter';
 import MultiSelectFilter from './FilterRow/filterInputs/multiSelectFilter/MultiSelectFilter';
 import { FilterProps } from './FilterRow/filterInputs/hooks';
 
-
 export interface FieldMapping {
-    nameProps: FilterProps<string>;
-    areaProps: FilterProps<String[]>;
+  nameProps: FilterProps<string>;
+  areaProps: FilterProps<String[]>;
 }
 
 interface FilterBarProps {
-    filters: FieldMapping;
+  filters: FieldMapping;
 }
 
-const FilterBar = ({filters}: FilterBarProps) => {
-    const {nameProps, areaProps} = filters;
+const FilterBar = ({ filters }: FilterBarProps) => {
+  const { nameProps, areaProps } = filters;
 
-    return <s.FilterBarContainer>
-        <s.FilterBarTitle>
-            <FilterIcon/>
-            &nbsp;
-            סינון מתנדבים
-        </s.FilterBarTitle>
-        <s.FilterBarRowsContainer>
-            <FilterRow {...nameProps}>
-                {(props) => <StringFilter {...props} />}
-            </FilterRow>
-            <FilterRow {...areaProps}>
-                {(props) => <MultiSelectFilter
-                    {...props}
-                    options={['מרכז', 'דרום', 'צפון']}
-                />}
-            </FilterRow>
-        </s.FilterBarRowsContainer>
-    </s.FilterBarContainer>;
-}
+  return (
+    <s.FilterBarContainer>
+      <s.FilterBarTitle>
+        <FilterIcon />
+        &nbsp; סינון מתנדבים
+      </s.FilterBarTitle>
+      <s.FilterBarRowsContainer>
+        <FilterRow {...nameProps}>{(props) => <StringFilter {...props} />}</FilterRow>
+        <FilterRow {...areaProps}>{(props) => <MultiSelectFilter {...props} options={['מרכז', 'דרום', 'צפון']} />}</FilterRow>
+      </s.FilterBarRowsContainer>
+    </s.FilterBarContainer>
+  );
+};
 
 export default FilterBar;
--- src/components/HomePage/HomePage.tsx
+++ src/components/HomePage/HomePage.tsx
@@ -3,7 +3,7 @@ import FilterBar from '../FilterBar';
 
 import { Home } from './HomePage.styles';
 import volunteerList from '../../mockData/volunteerList';
-import { useStringFilter } from '../FilterBar/FilterRow/filterInputs/StringFilter/stringFilterHook';
+import { useStringFilter } from '../FilterBar/FilterRow/filterInputs/stringFilter/stringFilterHook';
 import { useMultiSelectFilter } from '../FilterBar/FilterRow/filterInputs/multiSelectFilter/multiSelectFilterHook';
 import { FieldMapping } from '../FilterBar/FilterBar';
 import env from '../../env';
@@ -14,27 +14,24 @@ import { searchEntity } from '../../api/search';
 const HomePage = () => {
   const filters = getFilters();
   const filtersList = Object.values(filters)
-      .filter((filter: FilterProps<any>) => filter.isRelevant)
-      .map((filter: FilterProps<any>) => filter.toJson());
-  
+    .filter((filter: FilterProps<any>) => filter.isRelevant)
+    .map((filter: FilterProps<any>) => filter.toJson());
+
   const volunteersQuery = useQuery(
     ['volunteers', ...filtersList.map((filter: ApiFilter<any>) => JSON.stringify(filter.value))],
     () => searchEntity('volunteer', filtersList),
-    {enabled: true},
+    { enabled: true },
   );
-  console.log(volunteersQuery)
+  console.log(volunteersQuery);
   return (
     <Home>
-      <VolunteerList
-        volunteers={volunteerList}
-      />
+      <VolunteerList volunteers={volunteerList} />
       <FilterBar filters={filters} />
     </Home>
   );
 };
 
 export default HomePage;
-
 
 function getFilters(): FieldMapping {
   return {
