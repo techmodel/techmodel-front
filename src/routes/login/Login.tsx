b0728aa31ac3a9adf2cf7333e234bf8e90c409c0
Itay Houri <itay9263@gmail.com>
2022-08-27 23:58:16 +0300

    New login

--- src/routes/login/Login.tsx
+++ src/routes/login/Login.tsx
@@ -1,48 +1,59 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import axios from "axios"
-import { UnauthenticatedTemplate, useMsal } from '@azure/msal-react';
+import { UnauthenticatedTemplate, useAccount, useMsal } from '@azure/msal-react';
 import { useNavigate } from 'react-router-dom';
 import { loginRequest } from '../../config/auth.config';
-import { UserType } from '../../types/user'
-
+import { InteractionRequiredAuthError } from '@azure/msal-browser';
+import { UserType } from 'types/user';
 
 
 
 const Login = () => {
-    const { instance, accounts } = useMsal();
+    const callApiWithToken = (userId: string, accessToken: string) => {
+        const bearer = `Bearer ${accessToken}`
+        return axios.post("http://localhost:8080/api/v1/auth/login", { userId  }, { headers: { "Authorization": bearer } })
+    }
+    const { instance, accounts, inProgress } = useMsal();
     const navigate = useNavigate();
+    const account = useAccount(accounts[0] || {});
     useEffect(() => {
-
-        const getData = async () => {
+        const performLogin = async () => {
             try {
-
-                const response = await axios.post('http://localhost:8080/api/login', {
-                    userId: accounts[0].localAccountId
-                });
-                const { userDetails, isFound, userToken } = response.data;
-                const userType: keyof typeof UserType = response.data.userType;
-                if (!isFound) {
-                    navigate(`/register`, {state:{userInfo: accounts[0]}});
+                if (account && inProgress === "none") {
+                    const { idToken } = await instance.acquireTokenSilent({
+                        scopes: [],
+                        account
+                    })
+                    const { data } = await callApiWithToken(account.localAccountId, idToken);
+                    const { userDetails, isFound, userToken } = data;
+                    const userType: keyof typeof UserType = data.userType;
+                    if (!isFound) {
+                        navigate(`/register`, { state: { userInfo: accounts[0] } });
+                    }
+                    else {
+                        localStorage.setItem("token", userToken);
+                        //TODO: Add userDetails to Store
+                        navigate(`/${userType}`)
+                    }
                 }
-                else {
-                    localStorage.setItem("token", userToken);
-                    //TODO: Add userDetails to Store
-                    navigate(`/${userType}`)
+            }
+            catch (error) {
+                // in case if silent token acquisition fails, fallback to an interactive method
+                if (error instanceof InteractionRequiredAuthError) {
+                    //TODO: Do login again, this time with instance.acquireTokenPopup method
+                    
                 }
-            } catch (err) {
-                navigate(`/`)
             }
         }
-        if (accounts.length > 0) {
-            getData();
-        }
-    }, [])
+        performLogin()
+
+    }, [account, inProgress, instance]);
 
     return (
         <>
             <UnauthenticatedTemplate>
-                <button onClick={() => instance.loginRedirect(loginRequest)}>Login with google</button>
-                <button onClick={() => navigate(`/register`, {state:{userInfo: {email: "mynameis@gmail.com"}}})}>Register</button>
+                <button onClick={() => instance.loginPopup(loginRequest)}>Login with google</button>
+                <button onClick={() => navigate(`/register`, { state: { userInfo: { email: "mynameis@gmail.com" } } })}>Register</button>
             </UnauthenticatedTemplate>
         </>
     )
